43a930b21fd4b35692295ea2f9d7521fc8c035d6
CharlieBytesX <carlos.ace27@gmail.com>
2024-06-26 11:22:36 -0600

    feat: tanstack query instead of fetch

--- FE/src/pages/HomePage.tsx
+++ FE/src/pages/HomePage.tsx
@@ -5,38 +5,30 @@ import MainLayout from "../layouts/MainLayout";
 import CarCard, { type CardAd } from "@/components/shared/CarCard";
 import PageTitle from "@/components/shared/PageTitle";
 import { useNavigate } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
 
 export function HomePage() {
-  const [loading, setLoading] = useState(true);
   const [page, setPage] = useState(1);
-  const [data, setData] = useState<any>();
   const navigate = useNavigate();
-
-  useEffect(() => {
-    const load = async () => {
-      setLoading(true);
-      try {
-        const response = await fetch("/api/list_car_post");
-        const content = await response.json();
-        setData(content);
-        console.log(content);
-        setLoading(false);
-      } catch (error) {
-        console.log(error);
-        setLoading(true);
+  const { data, isPending } = useQuery({
+    queryKey: ["load_posts"],
+    queryFn: async () => {
+      const response = await fetch("/api/list_car_post");
+      const content = await response.json();
+      if (response.ok) {
+        return content;
       }
-    };
-    load();
-  }, []);
+    },
+  });
 
   return (
     <MainLayout>
       <PageTitle>Cars</PageTitle>
-      {loading && (
+      {isPending && (
         <RiseLoader className="mx-auto my-auto" color="darkturquoise" />
       )}
 
-      {!loading && (
+      {!isPending && (
         <div className="flex-1 mt-4 flex flex-wrap gap-2 pb-2 ">
           {data.map((ad: CardAd) => {
             return (
--- FE/src/pages/RegisterSuccesfull.tsx
+++ FE/src/pages/RegisterSuccesfull.tsx
@@ -1,22 +1,7 @@
-import { useMutation } from "@tanstack/react-query";
-import { useParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 export default function RegisterSuccesfull() {
-  const resendEmail = useMutation({
-    mutationFn: async (formData: FormData) => {
-      const res = await fetch("/api/sign_up", {
-        method: "POST",
-        body: formData,
-      });
-      if (res.status != 200) {
-        if (res.status == 400) {
-          let r = await res.json();
-          return r;
-        }
-        throw new Error("Pls try again later or contact support");
-      }
-    },
-  });
+  const navigator = useNavigate();
   return (
     <div className="flex flex-col bg-slate-100 items-center justify-center h-screen">
       <div className="bg-white border rounded-lg flex w-80   px-4 py-4 flex-col items-center justify-center">
@@ -26,7 +11,12 @@ export default function RegisterSuccesfull() {
         </p>
         <p className="text-xs mt-2">
           If you don't see the email pls
-          <button className="ml-1 font-semibold underline">click here</button>
+          <button
+            onClick={() => navigator("/resend_confirmation_email")}
+            className="ml-1 font-semibold underline"
+          >
+            click here
+          </button>
         </p>
       </div>
     </div>
